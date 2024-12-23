9d5c1033c00677e5c11fd35ff4073e37908c1aca
andulval <mzadlo@interia.pl>
2024-12-23 23:14:31 +0100

    build & test hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [
-    react(), // React plugin for Vite
-    svgr(), // SVG React component plugin
-  ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./test/setupTests.ts",
-    // You can add other vitest-related settings here
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(), // React plugin for Vite
+      svgr(), // SVG React component plugin
+    ],
+    ...(command === "serve" && {
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./test/setupTests.ts",
+        // You can add other vitest-related settings here
+      },
+    }),
+  };
 });
